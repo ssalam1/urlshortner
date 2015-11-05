698ba6b30f924ec5c43d2a7a792ebc79503cd167
Shadab Salam <ssalam@localhost.localdomain>
2015-11-05 23:51:20 +0530

    Form created

--- shortur/models.py
+++ shortur/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
 class urldata(models.Model):
-	url=models.URLField(max_length=200)
+	url=models.CharField(max_length=200)
 	url_short=models.SlugField()
 
 	"""docstring for urldata"models.Model """
+class urlform(forms.ModelForm):
+	"""docstring for urlfrom"""
+	class Meta:
+		model = urldata
+		fields=['url']
+		
--- shortur/views.py
+++ shortur/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render_to_response,render
+from django.http import HttpResponse,HttpRequest,HttpResponseRedirect
+from django.forms import ModelForm
+from shortur.models import *
 # Create your views here.
+def index(request):
+	newurl=urldata()
+	if request.method == "GET":
+		form = urlform()
+		return render (request, 'form.html',{'form':form})
+	elif request.method == "POST":
+		newurl.url=request.POST['url']
+#		newurl.url_short=shortner(newurl)
+		newurl.save()
+#		return HttpResponseRedirect('/index.html')
+	return render_to_response ('done.html')
+#def shortner(newurl):
