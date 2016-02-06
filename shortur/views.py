e65dc246222b34083fc9257b0d0f02d592306a4d
Shadab Salam <ssalam@localhost.localdomain>
2016-02-06 22:09:56 +0530

    [CHORE] added django vlidation

--- shortur/views.py
+++ shortur/views.py
@@ -6,25 +6,29 @@ from random import randint
 from django.shortcuts import get_object_or_404, get_list_or_404, render_to_response
 from django.shortcuts import redirect
 from django.contrib import messages 
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 import re
 # Create your views here.
 arr="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 def index(request):
-    reg_exp_url = re.compile('^(?!mailto:)(?:(?:http|https|ftp)://)?(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$')
     newurl=urldata()
     if request.method == "GET":
         form = urlform()
         return render (request, 'form.html',{'form':form})
     elif request.method == "POST":
-        if reg_exp_url.match(str(request.POST['url'])):
-			newurl.url=request.POST['url']
-			newurl.url_short=shortner(newurl)
-			newurl.save()
-			return render_to_response ('done.html',{'newurl':newurl})
-        else: 
-            messages.add_message(request, messages.INFO, 'Please Enter a Valid URL.')
-            return redirect('/')
+        val = URLValidator()
+        try :
+            val(request.POST['url'])
+        except ValidationError, e : 
+            messages.add_message(request, messages.INFO, e[0])
+            return redirect('/')        
+        newurl.url=request.POST['url']
+        newurl.url_short=shortner(newurl)
+        newurl.save()
+        return render_to_response ('done.html',{'newurl':newurl})
+
 
 def shortner(newurl):
     id = randint(0,9999999)
@@ -39,8 +43,6 @@ def notfound(request):
 def redirect_org(request,short_id):
 	current_url= get_object_or_404(urldata,url_short=short_id) # get object, if not found return 404 error
  	orgurl=current_url.url
- 	if orgurl[0:8] == 'https://' or orgurl[0:7]=='http://':
- 		return redirect(orgurl)
- 	else:
- 		return redirect('https://'+orgurl)
+ 	return redirect(orgurl)
+ 
   
